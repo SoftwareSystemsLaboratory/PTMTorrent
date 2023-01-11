a18ed7b1472874ea152840fa05446202e6fd347c
Taylor Schorlemmer <taylor.schorlemmer@gmail.com>
2023-01-11 23:56:34 +0000

    add basic functionality for 1_getList

--- PHTorrent/scripts/0_setup.bash
+++ PHTorrent/scripts/0_setup.bash
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# 0_setup.bash
+# Author: Taylor R. Schorlemmer
+# Creates a virtual environment for execution
+# Creates folders for intermediate and final files later on.
+
 echo "Creating virtual environment..."
 python3 -m venv env
 
 ./env/bin/python -m pip install --upgrade pip
 ./env/bin/python -m pip install -r requirements.txt
+
+
+echo "Creating directories..."
+mkdir ../{html,json,repos}
 
--- PHTorrent/scripts/run.bash
+++ PHTorrent/scripts/run.bash
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# run.bash
+# Author: Taylor R. Schorlemmer
+# Sequentially executes each bash file in the scripts folder.
+
 ./0_setup.bash
+./1_getList.bash
