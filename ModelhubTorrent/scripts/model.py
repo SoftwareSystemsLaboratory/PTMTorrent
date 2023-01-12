356825a87c76822329d0da18cb2907b2ebcd4ed2
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2023-01-12 09:57:56 +0530

    feat: add modelhub related metadata

---
+++ ModelhubTorrent/scripts/model.py
@@ -0,0 +1,48 @@
+"""
+Helpers for converting a fetched model info  to the desired schema
+"""
+from model_types import TModelHubConfig, TModelSchema
+
+
+class Model:
+    """
+    Model class to generate a dictionary matching the required schema
+    """
+
+    def numeric_id(self, arg: str):
+        """
+        convert a string based id to numeric
+        I hope there's no collisions here
+        """
+
+        return hash(arg)
+
+    def __init__(self, config: TModelHubConfig, path: str, url: str) -> None:
+        self.config = config
+        self.path = path
+        self.url = url
+
+    @property
+    def as_json(self) -> TModelSchema:
+        """
+        converts a modelhub config type to the schema
+        """
+        return {
+            "id": self.numeric_id(self.config["id"]),
+            "Datasets": [
+                # modelhub does not provide dataset information
+            ],
+            "LatestGitCommitSHA": "TODO",
+            "ModelArchitecture": self.config["model"]["architecture"],
+            "ModelHub": {
+                "ModelHubName": "modelhub (modelhub.ai)",
+                "MetadataFilePath": str(self.path),
+                "MetadataObjectID": self.numeric_id(self.config["id"]),
+            },
+            "ModelName": self.config["meta"]["name"],
+            "ModelOwner": self.config["publication"]["authors"],
+            "ModelOwnerURL": self.config["publication"]["google_scholar"],
+            "ModelPaperDOI": self.config["publication"]["url"],
+            "ModelTask": self.config["meta"]["task"],
+            "ModelURL": self.url,
+        }
