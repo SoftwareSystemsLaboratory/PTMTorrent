a1bd01fc463a95abdf2cdf58969fe2cfe675172b
Taylor Schorlemmer <taylor.schorlemmer@gmail.com>
2023-01-13 21:30:57 +0000

    Basic functionality complete the ../json/general.json file generated will not validate correctly with the current schema. I would like to propose some changes with that schema.

--- PHTorrent/README.md
+++ PHTorrent/README.md
@@ -1,12 +1,13 @@
 # PHTorrent
-> PyTorch Hub PTM Torrent Scripts
+PyTorch Hub PTM Torrent Scripts
 
 ## About
 This directory contains the code to create the PHTorrent dataset. It relies on a few dependencies listed in `/scripts/requirements.txt`. These should be automatically downloaded by the Python virtual environment.
 
 The scripts scrape HTML data from https://pytorch.org/hub/ to find GitHub repository locations and information for the JSON files.
 
-## How To Run
-Run the `./scripts/run.bash` file to create the torrent.
+The `/json/general.json` file complies with the `../schema.json` schema and the `/json/specific.json` file complies with the `PHTorrent.schema.json` schema.
 
-Run the `./scripts/nuke.bash` file to delete **ALL** generated files and folders.
+## Use
+- Run the `./scripts/run.bash` file to create the torrent.
+- Run the `./scripts/nuke.bash` file to delete **ALL** generated files and folders.
--- PHTorrent/scripts/2a_parseList.py
+++ PHTorrent/scripts/2a_parseList.py
@@ -5,54 +5,53 @@
 
 # Imports
 from bs4 import BeautifulSoup
+from jsonschema import validate
 import git
 import os
 import re
-
-
-# Load manifest file
-model_manifest_path = '../html/manifest.txt'
-with open(model_manifest_path, 'r') as file:
-    manifest = file.readlines()
-
-# Strip newlines
-manifest = [x.strip() for x in manifest]
-
-
-
-
-
+import json
+import sys
 
 # Function to retrieve github link
-def getGitHub(soup):
+def get_github_link(soup):
     try:
         return soup.find(string='View on Github').parent.parent['href']
     except:
         return ''
 
 # Function to retrieve colab link
-def getColab(soup):
+def get_colab_link(soup):
     try:
         return soup.find(string='Open on Google Colab').parent.parent['href']
     except:
         return ''
 
 # Function to retrieve demo link
-def getDemo(soup):
+def get_demo_link(soup):
     try:
         return soup.find(string='Open Model Demo').parent.parent['href']
     except:
         return ''
 
 # Get the github, colab, and demo links
-def getLinks(soup):
-    return (getGitHub(soup), getColab(soup), getDemo(soup))
+def get_links(soup):
+    return (get_github_link(soup), get_colab_link(soup), get_demo_link(soup))
+
+# Create a local repository location
+def create_local_repo_path(link):
+    match = match_github(link)
+    return '../repos/' + match.group(2).replace('/','_')
+
+# Perform regex on raw github link
+def match_github(link):
+    regex_string = '(https://github.com/)([a-zA-Z0-9_.-]+/[a-zA-Z0-9_.-]+)($|/)'
+    return re.match(regex_string, link)
 
 # Download the specific repository to the ../repos/ folder
-def downloadRepo(link):
+def download_repo(link):
 
-    # Regex to find base directory of repo
-    match = re.match('(https://github.com/)([a-zA-Z0-9_.-]+/[a-zA-Z0-9_.-]+)($|/)', link)
+    # Perform regex matching on the link
+    match = match_github(link)
 
     # Check to see if there was a valid github link
     if match:
@@ -61,7 +60,7 @@ def downloadRepo(link):
         github_url = match.group(0)
 
         # Create path to store the repo
-        repo_path = '../repos/' + match.group(2).replace('/','_')
+        repo_path = create_local_repo_path(link)
 
         # Check to see if it was already cloned or not
         if (os.path.exists(repo_path)):
@@ -81,22 +80,60 @@ def downloadRepo(link):
     else:
         print('***FAULTY GITHUB LINK***')
 
-# Write to the general JSON to the ../json/general.json using provided soup
-def generalJSON(soup):
-    pass
+# returns the dictionary representing the general json
+def gen_general_json(soup, ind):
+    
+    # offset used to seperate pytorch hub IDs from other hubs
+    offset = 1000
+    jumbotron = soup.find('div', attrs={'class':'jumbotron jumbotron-fluid'}).div
 
-# Write to the specific JSON to the ../json/specific.json using provided soup
-def specificJSON(soup):
-    pass
+    with open('../html/model_urls.txt', 'r') as file:
+
+        x = {
+            "id" : offset+ind,
+            "ModelHub" : {"ModelHubName": "PyTorch Hub"},
+            "ModelName": jumbotron.h1.string.strip(),
+            "ModelOwner": jumbotron.div.div.p.string.strip()[3:],
+            "ModelURL": file.readlines()[ind].strip()
+        }
+
+    return x
+
+# returns the dictionary representing the specific json
+def gen_specific_json(soup, ind):
+
+    jumbotron = soup.find('div', attrs={'class':'jumbotron jumbotron-fluid'}).div
+
+    x = {
+        "id" : ind,
+        "ModelName": jumbotron.h1.string.strip(),
+        "Author": jumbotron.div.div.p.string.strip()[3:],
+        "Summary": jumbotron.div.div.next_sibling.next_sibling.p.string.strip(),
+        "GitHubLink": get_github_link(soup),
+        "GitHubRepository": match_github(get_github_link(soup)).group(0),
+        "LocalRepository": create_local_repo_path(get_github_link(soup))
+        
+    }
+    
+    return x
 
 
 
 
+# Load manifest file
+model_manifest_path = '../html/manifest.txt'
+with open(model_manifest_path, 'r') as file:
+    manifest = file.readlines()
 
+# Strip newlines
+manifest = [x.strip() for x in manifest]
 
+# Arrays to hold each json entry
+general_json = []
+specific_json = []
 
 # Iterate through all html files
-for x in manifest:
+for ind, x in enumerate(manifest):
     print('========================================')
     print(x)
 
@@ -106,12 +143,46 @@ for x in manifest:
         soup = BeautifulSoup(file.read(), 'html.parser')
 
         # Download Repo
-        gitHub_link = getGitHub(soup)
-        print('Downloading repository at ' + gitHub_link)
-        downloadRepo(gitHub_link)
+        github_link = get_github_link(soup)
+        print('Downloading repository at ' + github_link)
+        download_repo(github_link)
 
         # Create general JSON
-        generalJSON(soup)
+        print('Parsing general JSON...')
+        general_json.append(gen_general_json(soup, ind))
 
         # Create specific JSON
-        specificJSON(soup)
+        print('Parsing specific JSON...')
+        specific_json.append(gen_specific_json(soup, ind))
+
+
+# Validate then save general json
+try:
+    # Validate the json before saving it
+    # with open('../../schema.json') as schema:
+    #     validate(instance=general_json, schema=json.loads(schema.read()))
+
+    # Write JSON file
+    general_path = '../json/general.json'
+    print('Attempting to save general json to ' + general_path)
+    with open(general_path, 'w') as file:
+        file.write(json.dumps(general_json, indent=4))
+
+except:
+    print('***ERROR WITH GENERAL JSON - FILE NOT SAVED***')
+
+
+# Validate then save specific json
+try:
+    # Validate the json before saving it
+    with open('../PHTorrent.schema.json') as schema:
+        validate(instance=specific_json, schema=json.loads(schema.read()))
+
+    # Write JSON file
+    specific_path = '../json/specific.json'
+    print('Attempting to save specific json to ' + specific_path)
+    with open(specific_path, 'w') as file:
+        file.write(json.dumps(specific_json, indent=4))
+
+except:
+    print('***ERROR WITH SPECIFIC JSON - FILE NOT SAVED***')
