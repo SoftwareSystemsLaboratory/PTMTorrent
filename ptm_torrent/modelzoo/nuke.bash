b8b2443dd04377cfa14c77abd91a3397b37081da
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-01-18 14:38:00 -0600

    Delete files

--- ptm_torrent/modelzoo/nuke.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# WARNING: This delets all data that has been created by the scripts in this folder.
-# You will have to recreate all of the data after running this script
-
-rm -r ../{html,json,repos}
-rm model_links, git_links, model_names
-yes | rm -r env
--- ptm_torrent/modelzoo/run.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./0_setupScripts.bash
-./1_setupMZTorrent.bash
-./2_downloadModelPage.bash
-./3_getModelMetadata.bash
-./4_gitCloneModels.bash
