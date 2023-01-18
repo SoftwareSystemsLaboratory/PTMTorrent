e018dc6b26f1a735e35954f989994054d097d035
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-01-18 14:39:21 -0600

    Add file to setup the model zoo file storage

--- ptm_torrent/modelzoo/0_setupChromeDriver.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-
-if __name__ == "__main__":
-    driver = webdriver.Chrome(ChromeDriverManager().install())
