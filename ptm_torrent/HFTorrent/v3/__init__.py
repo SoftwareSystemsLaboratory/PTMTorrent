f055afd18e1da5ee74ea3c4e9de32444e38108ad
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-01-23 10:48:46 -0600

    Copy path generation code across repos

--- ptm_torrent/HFTorrent/v3/__init__.py
+++ ptm_torrent/HFTorrent/v3/__init__.py
@@ -1,23 +1,25 @@
 from pathlib import PurePath
+from typing import List
 
 import ptm_torrent as pt
 
-rootFolderPath: PurePath = PurePath(f"{pt.dataFolderPath}/huggingface")
+rootFolderPath: PurePath = PurePath(f"{pt.dataFolderPath}/modelhub")
 
 reposFolderPath: PurePath = PurePath(f"{rootFolderPath}/{pt.reposFolderPath}")
 
-jsonFolderPath: PurePath = PurePath(f"{rootFolderPath}/{pt.jsonFolderPath}")
-jsonMetadataFolderPath: PurePath = PurePath(
-    f"{rootFolderPath}/{pt.jsonMetadataFolderPath}"
-)
-jsonModelMetadataFolderPath: PurePath = PurePath(
-    f"{rootFolderPath}/{pt.jsonModelMetadataFolderPath}"
-)
+jsonFolderPath: PurePath = PurePath(f"{pt.jsonFolderPath}")
+jsonMetadataFolderPath: PurePath = PurePath(f"{pt.jsonMetadataFolderPath}")
+jsonModelMetadataFolderPath: PurePath = PurePath(f"{pt.jsonModelMetadataFolderPath}")
 
-htmlFolderPath: PurePath = PurePath(f"{rootFolderPath}/{pt.htmlFolderPath}")
-htmlMetadataFolderPath: PurePath = PurePath(
-    f"{rootFolderPath}/{pt.htmlMetadataFolderPath}"
-)
-htmlModelMetadataFolderPath: PurePath = PurePath(
-    f"{rootFolderPath}/{pt.htmlModelMetadataFolderPath}"
-)
+htmlFolderPath: PurePath = PurePath(f"{pt.htmlFolderPath}")
+htmlMetadataFolderPath: PurePath = PurePath(f"{pt.htmlMetadataFolderPath}")
+htmlModelMetadataFolderPath: PurePath = PurePath(f"{pt.htmlModelMetadataFolderPath}")
+
+subFolders: List[PurePath] = [
+    jsonFolderPath,
+    jsonMetadataFolderPath,
+    jsonModelMetadataFolderPath,
+    htmlFolderPath,
+    htmlMetadataFolderPath,
+    htmlModelMetadataFolderPath,
+]
