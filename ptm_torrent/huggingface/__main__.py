b47491beaa71fb1bf4a9967aa483a7b49c235e41
Taylor Schorlemmer <taylor.schorlemmer@gmail.com>
2023-01-24 20:06:26 +0000

    Add create schema

--- ptm_torrent/huggingface/__main__.py
+++ ptm_torrent/huggingface/__main__.py
@@ -1,4 +1,4 @@
-# import ptm_torrent.huggingface.createSchema as createSchema
+import ptm_torrent.huggingface.createSchema as createSchema
 import ptm_torrent.huggingface.downloadJSON as downloadJSON
 import ptm_torrent.huggingface.downloadRepos as downloadRepos
 import ptm_torrent.huggingface.setupFileSystem as setupFS
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     setupFS.main()
     downloadJSON.main()
     downloadRepos.main()
-    # createSchema.main()
+    createSchema.main()
