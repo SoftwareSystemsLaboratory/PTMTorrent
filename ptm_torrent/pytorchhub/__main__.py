ea5b3771fb934864c6efc19f56aa639910657755
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-01-23 20:11:11 -0600

    Rename folder

---
+++ ptm_torrent/pytorchhub/__main__.py
@@ -0,0 +1,10 @@
+# import ptm_torrent.huggingface.createSchema as createSchema
+# import ptm_torrent.pytorchhub.downloadJSON as downloadJSON
+# import ptm_torrent.pytorchhub.downloadRepos as downloadRepos
+import ptm_torrent.pytorchhub.setupFileSystem as setupFS
+
+if __name__ == "__main__":
+    setupFS.main()
+    # downloadJSON.main()
+    # downloadRepos.main()
+    # createSchema.main()
