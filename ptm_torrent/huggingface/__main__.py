f0bdef94e38339c0d814068ac107de3860cf6d0a
Nicholas Synovic <nicholas.synovic@gmail.com>
2023-01-24 19:46:10 -0600

    Update __main__.py

--- ptm_torrent/huggingface/__main__.py
+++ ptm_torrent/huggingface/__main__.py
@@ -3,8 +3,10 @@ import ptm_torrent.huggingface.downloadJSON as downloadJSON
 import ptm_torrent.huggingface.downloadRepos as downloadRepos
 import ptm_torrent.huggingface.setupFileSystem as setupFS
 
+shrinkage: float = 0.1
+
 if __name__ == "__main__":
     setupFS.main()
     downloadJSON.main()
-    downloadRepos.main()
+    downloadRepos.main(shrinkage)
     # createSchema.main()
