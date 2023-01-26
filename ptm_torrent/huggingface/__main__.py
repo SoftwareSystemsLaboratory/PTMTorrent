d468f4415e1b87e86a9be108426a8f8fd3b95df8
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-01-26 10:35:14 -0600

    Format code to be pythonic

--- ptm_torrent/huggingface/__main__.py
+++ ptm_torrent/huggingface/__main__.py
@@ -5,25 +5,25 @@ import ptm_torrent.huggingface.downloadJSON as downloadJSON
 import ptm_torrent.huggingface.downloadRepos as downloadRepos
 import ptm_torrent.huggingface.setupFileSystem as setupFS
 
-shrinkage: float | str | None
-
-shrinkageEnvironmentVariable: str = "HF_TORRENT_SHRINKAGE"
-shrinkageEnvironmentValue: str = environ.get(shrinkageEnvironmentVariable)
-
-if shrinkageEnvironmentValue is None:
-    shrinkage = 0.1
-
-try:
-    shrinkage = float(shrinkageEnvironmentValue)
-except ValueError:
-    print(f"Invalid value for {shrinkageEnvironmentVariable}. Defaulting to 0.1")
-    shrinkage = 0.1
-
-if shrinkage > 1 or shrinkage <= 0:
-    print(f"Invalid value for {shrinkageEnvironmentVariable}. Defaulting to 0.1")
-    shrinkage = 0.1
-
 if __name__ == "__main__":
+    shrinkage: float | str | None
+
+    shrinkageEnvironmentVariable: str = "HF_TORRENT_SHRINKAGE"
+    shrinkageEnvironmentValue: str = environ.get(shrinkageEnvironmentVariable)
+
+    if shrinkageEnvironmentValue is None:
+        shrinkage = 0.1
+
+    try:
+        shrinkage = float(shrinkageEnvironmentValue)
+    except ValueError:
+        print(f"Invalid value for {shrinkageEnvironmentVariable}. Defaulting to 0.1")
+        shrinkage = 0.1
+
+    if shrinkage > 1 or shrinkage <= 0:
+        print(f"Invalid value for {shrinkageEnvironmentVariable}. Defaulting to 0.1")
+        shrinkage = 0.1
+
     setupFS.main()
     downloadJSON.main()
     downloadRepos.main(shrinkage)
